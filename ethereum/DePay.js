78b9897a82dd2249f23dce11fd4a2cde14525e42
Abishek Upadhyay <98577878+abishek023@users.noreply.github.com>
2023-03-03 15:43:25 +0530

    Delete ethereum directory

--- ethereum/DePay.js
+++
@@ -1,9 +0,0 @@
-import web3 from "./web3";
-import DePayContract from "./build/DePay.json";
- 
-const instance = new web3.eth.Contract(
-  DePayContract.abi,
-"0x3b5Ca02f7aD0dA59aAb25666a109A4ae39CC8D90"
-);
- 
-export default instance;
