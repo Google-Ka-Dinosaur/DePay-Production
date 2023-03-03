663a2eacd3f3719c8031ad57f03984149bfb0973
Abishek Upadhyay <98577878+abishek023@users.noreply.github.com>
2023-03-03 17:17:10 +0530

    Delete DePaydual.js

--- ethereum/DePaydual.js
+++
@@ -1,9 +0,0 @@
-import web3 from "./web3";
-import DePaydualContract from "./build/DePaydual.json";
- 
-const instance = new web3.eth.Contract(
-  DePaydualContract.abi,
-"0x8bfd01328e90E768363fbf0E047EA34e7E3f969D"
-);
- 
-export default instance;
