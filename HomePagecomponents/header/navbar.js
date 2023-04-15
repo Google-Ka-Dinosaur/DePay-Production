efe851c64d79c9e02f709802db42e3b245330287
Abishek Upadhyay <98577878+Google-Ka-Dinosaur@users.noreply.github.com>
2023-04-15 22:33:19 +0530

    Update navbar.js

--- HomePagecomponents/header/navbar.js
+++ HomePagecomponents/header/navbar.js
@@ -12,12 +12,14 @@ const NavBar = props =>{
     const change=()=>{
         router.push('./');
     }
+    // <a href="https://ibb.co/9qVRsFK"><img src="https://i.ibb.co/9qVRsFK/Google-Cloud-Emblem-removebg-preview.png" alt="Google-Cloud-Emblem-removebg-preview" border="0"></a>
 // console.log(props.connected);
     return(
         <>
         <div className = {classes.nav}>
             <div className={classes.box}>
                 <img onClick={change} className={classes.image} src="https://i.ibb.co/ftkmQvB/De-Pay-removebg-preview.png"  alt="logo" border="0" />
+                <a href="https://cloud.google.com/free?userloc_9154876-network_g" target='blank'><img className={`${classes.image} ${classes.pdt}`} src="https://i.ibb.co/9qVRsFK/Google-Cloud-Emblem-removebg-preview.png" alt="Google-Cloud-Emblem-removebg-preview" border="0"/></a>
             </div>
             <div className={classes.box}>
                {!props.connected&& <button onClick={props.connect} className={classes.button}>Connect</button>}
