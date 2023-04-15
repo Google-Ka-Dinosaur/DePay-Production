58f6461ccd8eb4617ca0cff503b2df8b426d88d5
Abishek Upadhyay <98577878+Google-Ka-Dinosaur@users.noreply.github.com>
2023-04-15 22:32:16 +0530

    Update icons.js

--- HomePagecomponents/footer/icons.js
+++ HomePagecomponents/footer/icons.js
@@ -5,11 +5,11 @@ import classes from './icons.module.css'
 export default function Icons() {
   return (
     <div className={classes.icons}>
-    <div><SocialIcon url='https://www.facebook.com/advaita.iiit/' style={{ height: 40, width: 40 }}></SocialIcon></div>
-    <div><SocialIcon url='https://www.linkedin.com/company/iiit-bhubaneswar/' style={{ height: 40, width: 40 }}></SocialIcon></div>
+    <div><SocialIcon url='https://www.facebook.com/Google/' style={{ height: 40, width: 40 }}></SocialIcon></div>
+    <div><SocialIcon url='https://www.linkedin.com/company/google/?originalSubdomain=in' style={{ height: 40, width: 40 }}></SocialIcon></div>
     <div><SocialIcon url='https://github.com/abishek023/DePay' style={{ height: 40, width: 40 }}></SocialIcon></div>
-    <div><SocialIcon url='https://www.instagram.com/advaita_iiitbh/' style={{ height: 40, width: 40 }}></SocialIcon></div>
-    <div><SocialIcon url="https://advaita-iiitbh.in/" style={{ height: 40, width: 40 }}></SocialIcon></div>
+    <div><SocialIcon url='https://www.instagram.com/google/?hl=en' style={{ height: 40, width: 40 }}></SocialIcon></div>
+    <div><SocialIcon url="https://cloud.google.com/free?userloc_9154876-network_g" style={{ height: 40, width: 40 }}></SocialIcon></div>
     </div>
   );
 }
